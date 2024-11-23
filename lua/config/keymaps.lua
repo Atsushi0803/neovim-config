e52e64448f0bd06b1a33f90fdec7bdac1fd69625
Atsushi0803 <atsushi.7624@gmail.com>
2024-11-23 15:48:31 +0900

    Gtagのkeymapを追加

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -15,3 +15,17 @@ vim.api.nvim_set_keymap('v', '<Home>', '', { callback = smart_home })
 vim.api.nvim_set_keymap('i', 'jj', '<ESC>', {silent=true})
 -- vim.api.nvim_set_keymap('v', 'jj', '<ESC>', {silent=true})
 
+vim.keymap.set("n", "gd", function()
+  -- 現在のカーソル位置にある単語を取得
+  local word = vim.fn.expand("<cword>")
+  -- :Gtags コマンドを実行
+  vim.cmd("Gtags " .. word)
+end, { noremap = true, silent = true })
+
+vim.keymap.set("n", "gr", function()
+  -- 現在のカーソル位置にある単語を取得
+  local word = vim.fn.expand("<cword>")
+  -- :Gtags -r コマンドを実行
+  vim.cmd("Gtags -r " .. word)
+end, { noremap = true, silent = true })
+
